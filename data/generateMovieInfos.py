e7df94e6477751de2a9559d1b81bc9322dc31249
Jerome Le Lan <lelan.jerome@gmail.com>
2017-03-20 23:52:28 -0400

    Add table of movies + parsing of actors, directors and cinematographers

--- data/generateMovieInfos.py
+++ data/generateMovieInfos.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys, time, json, urllib2, math, collections
+import sys, time, json, urllib2, math, collections, operator
 
 apiFile = "../private/themoviedb.apikey"
-watchedFile = "watched.txt"
-#watchedFile = "watchedSample.txt" // debug
-exportFile = "../js/watched.json"
 
 def getTheMovieDBApiKey():
 	with open(apiFile, "rU") as f:
@@ -13,40 +10,102 @@ def getTheMovieDBApiKey():
 			apiKey = line
 	return apiKey
 
+apiKey = getTheMovieDBApiKey()
+
+watchedFile = "watched.txt"
+#watchedFile = "watchedSample.txt" # debug
+exportFile = "../js/watched.json"
+
+movieInfos = []
+countryList = dict()
+decadeList = dict()
+genreList = dict()
+actorList = dict()
+directorList = dict()
+cameraList = dict()
+jsonResult = dict()
+
+def addItemToDict(vDict, key, value, valueType):
+	if not key in vDict:
+		vDict[key] = dict()
+		vDict[key]["count"] = 0
+		vDict[key][valueType] = value
+	vDict[key]["count"] = vDict[key]["count"] + 1
+
+def getMovieInfo(imdbId):
+	print imdbId,
+	url = "https://api.themoviedb.org/3/movie/tt" + imdbId + \
+		"?api_key=" + apiKey + "&language=en-US"
+	movieInfo = json.load(urllib2.urlopen(url))
+	time.sleep(0.25)
+	print "-> " + movieInfo["original_title"].encode('utf-8'),
+	return movieInfo
+
+def getCreditsInfo(imdbId):
+	url = "https://api.themoviedb.org/3/movie/tt" + imdbId + \
+		"/credits?api_key=" + apiKey
+	credits = json.load(urllib2.urlopen(url))
+	time.sleep(0.25)
+	director = ""
+	for cast in credits["cast"]:
+		addItemToDict(actorList, cast["id"], cast["name"], "name")
+	for crew in credits["crew"]:
+		if crew["job"] == "Director":
+			director += crew["name"] + ", "
+			addItemToDict(directorList, crew["id"], crew["name"], "name")
+		if crew["job"] == "Director of Photography":
+			addItemToDict(cameraList, crew["id"], crew["name"], "name")
+	return director[0:len(director)-2]
+
+def getDecade(movieInfo):
+	decade = str(int(math.floor(int(movieInfo["release_date"][0:4]) / 10) * 10)) + 's'
+	print "- " + decade
+	decadeList[decade] = decadeList.get(decade, 0) + 1
+
+def getCountry(movieInfo):
+	for country in movieInfo["production_countries"]:
+		countryISO = country["iso_3166_1"].replace("SU", "RU")
+		countryName = country["name"].replace("Soviet Union", "Russia")
+		addItemToDict(countryList, countryISO, countryName, "name")
+
+def getGenre(movieInfo):
+	for genre in movieInfo["genres"]:
+		genreName = genre["name"]
+		genreList[genreName] = genreList.get(genreName, 0) + 1
+
 def generateMovieInfos():
-	apiKey = getTheMovieDBApiKey()
-	movieInfos = []
-	countryStats = dict()
-	decadeStats = dict()
-	genreStats = dict()
-	jsonResult = dict()
 	with open(watchedFile, "rU") as f:
 		for movie in f:
 			watchDate, imdbId, rating = movie.strip().split(" ")
 			if movie:
-				print imdbId,
-				url = "https://api.themoviedb.org/3/movie/tt" + imdbId + "?api_key=" + apiKey + "&language=en-US"
-				response = json.load(urllib2.urlopen(url))
-				print "-> " + response["original_title"].encode('utf-8'),
-				response["watchDate"] = watchDate
-				response["personalRating"] = rating
-				movieInfos.append(response)
-				decade = str(int(math.floor(int(response["release_date"][0:4]) / 10) * 10)) + 's'
-				print "- " + decade
-				decadeStats[decade] = decadeStats.get(decade, 0) + 1
-				for country in response["production_countries"]:
-					countryISO = country["iso_3166_1"]
-					countryISO = countryISO.replace("SU", "RU")
-					countryStats[countryISO] = countryStats.get(countryISO, 0) + 1
-				for genre in response["genres"]:
-					genreName = genre["name"]
-					genreStats[genreName] = genreStats.get(genreName, 0) + 1
+				movieInfo = getMovieInfo(imdbId)
+				movieInfo["watchDate"] = watchDate
+				movieInfo["personalRating"] = rating
+				getDecade(movieInfo)
+				getCountry(movieInfo)
+				getGenre(movieInfo)
+				director = getCreditsInfo(imdbId)
+				movieInfo["director"] = director
+				movieInfos.append(movieInfo)
 				sys.stdout.flush()
-				time.sleep(0.3)
-	jsonResult["countries"] = countryStats
-	decadeStatsSorted = collections.OrderedDict(sorted(decadeStats.items()))
-	jsonResult["decades"] = decadeStatsSorted
-	jsonResult["genres"] = genreStats
+
+	jsonResult["countries"] = countryList
+
+	decadeListSorted = collections.OrderedDict(sorted(decadeList.items()))
+	jsonResult["decades"] = decadeListSorted
+
+	genreListSorted = sorted(genreList.items(), key=operator.itemgetter(1), reverse=True)
+	jsonResult["genres"] = collections.OrderedDict(genreListSorted)
+
+	actorListSorted = sorted(actorList.items(), key=operator.itemgetter(1), reverse=True)
+	jsonResult["actors"] = actorListSorted[0:10]
+
+	directorListSorted = sorted(directorList.items(), key=operator.itemgetter(1), reverse=True)
+	jsonResult["directors"] = directorListSorted
+
+	cameraListSorted = sorted(cameraList.items(), key=operator.itemgetter(1), reverse=True)
+	jsonResult["cinematographers"] = cameraListSorted[0:10]
+
 	jsonResult["movies"] = movieInfos
 	file = open(exportFile, "w")
 	file.write(json.dumps(jsonResult, indent=4, separators=(',', ': ')))
--- js/main.js
+++ js/main.js
@@ -1,70 +1,104 @@
 function drawRegionsMap() {
-    var jsonData = $.ajax({
-        url: 'js/watched.json',
-        dataType: 'json',
-    }).done(function (results) {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Country');
-        data.addColumn('number', 'Movie count');
+  var jsonData = $.ajax({
+    url: 'js/watched.json',
+      dataType: 'json',
+  }).done(function (results) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Country');
+    data.addColumn('number', 'Movie count');
 
-        $.each(results.countries, function(index, value) {
-            data.addRow([index,value]);
-        });
-
-        var options = {
-            colorAxis: {colors: ['#fdfd96', '#fadadd', '#db7093'], values: [1, 30, 150]},
-            backgroundColor: '#b0e0e6',
-        };
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-        chart.draw(data, options);
+    $.each(results.countries, function(index, value) {
+      data.addRow([value.name,value.count]);
     });
+
+    var options = {
+      colorAxis: {
+      colors: ['#B6C9D9', '#97BDDB', '#86B5DB', '#2E8CD9'],
+        values: [1, 10, 30, 150]
+      },
+      backgroundColor: '#DAE5ED',
+    };
+    var chart = new google.visualization.GeoChart(document.getElementById('countries'));
+    chart.draw(data, options);
+  });
 }
 
 function drawYearChart() {
-    var jsonData = $.ajax({
-        url: 'js/watched.json',
-        dataType: 'json',
-    }).done(function (results) {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Decade');
-        data.addColumn('number', 'Movie count');
+  var jsonData = $.ajax({
+    url: 'js/watched.json',
+    dataType: 'json',
+  }).done(function (results) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Decade');
+    data.addColumn('number', 'Movie count');
 
-        $.each(results.decades, function(index, value) {
-            data.addRow([index,value]);
-        });
-
-        var options = {
-            legend: { position: 'none' },
-            colors:['#db7093'],
-        };
-        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
+    $.each(results.decades, function(index, value) {
+      data.addRow([index,value]);
     });
+
+    var options = {
+      legend: { position: 'none' },
+      colors:['#db7093'],
+    };
+    var chart = new google.visualization.BarChart(document.getElementById('decades'));
+    chart.draw(data, options);
+  });
 }
 
 function drawGenreChart() {
-    var jsonData = $.ajax({
-        url: 'js/watched.json',
-        dataType: 'json',
-    }).done(function (results) {
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Genre');
-        data.addColumn('number', 'Movie count');
+  var jsonData = $.ajax({
+    url: 'js/watched.json',
+    dataType: 'json',
+  }).done(function (results) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Genre');
+    data.addColumn('number', 'Movie count');
 
-        $.each(results.genres, function(index, value) {
-            data.addRow([index,value]);
-        });
-
-        var options = {
-            pieHole: 0.3,
-            sliceVisibilityThreshold: .03
-        };
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
+    $.each(results.genres, function(index, value) {
+      data.addRow([index,value]);
     });
+
+    var options = {
+      pieHole: 0.3,
+      sliceVisibilityThreshold: .03
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('genres'));
+    chart.draw(data, options);
+  });
 }
 
 google.charts.load('current', {'packages':['geochart', 'corechart']});
 google.charts.setOnLoadCallback(drawRegionsMap);
 google.charts.setOnLoadCallback(drawYearChart);
 google.charts.setOnLoadCallback(drawGenreChart);
+
+$(document).ready(function() {
+  var jsonData = $.ajax({
+    url: 'js/watched.json',
+    dataType: 'json',
+  }).done(function (results) {
+    var dataSet = [];
+    $.each(results.movies, function(index, value) {
+      dataSet.push([
+        value.watchDate,
+        value.original_title,
+        value.director,
+        value.release_date,
+        value.runtime,
+        value.personalRating
+      ]);
+    });
+    $('#movieList').DataTable({
+      data: dataSet,
+      order: [[ 0, "desc" ]],
+      columns: [
+        { title: "Watched on" },
+        { title: "Title" },
+        { title: "Director" },
+        { title: "Year" },
+        { title: "Runtime" },
+        { title: "Rating" }
+      ]
+    });
+  });
+});
