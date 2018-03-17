02cfbd609bcdf672c7c8e9a8166d5fe4d85836a9
Jerome Le Lan <lelan.jerome@gmail.com>
2018-03-17 11:44:50 -0400

    Update movie list + Python 2 -> 3

--- data/generateMovieInfos.py
+++ data/generateMovieInfos.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys, time, json, urllib2, math, collections, operator
+import sys, time, json, urllib.request, math, collections, operator
 
 apiFile = "../private/themoviedb.apikey"
 
@@ -35,14 +35,18 @@ def addItemToDict(vDict, key, value, valueType):
 def getMovieInfo(imdbId):
 	url = "https://api.themoviedb.org/3/movie/tt" + imdbId + \
 		"?api_key=" + apiKey + "&language=en-US"
-	movieInfo = json.load(urllib2.urlopen(url))
+	request = urllib.request.Request(url)
+	response = urllib.request.urlopen(request)
+	movieInfo = json.load(response)
 	time.sleep(0.25)
 	return movieInfo
 
 def getCreditsInfo(imdbId):
 	url = "https://api.themoviedb.org/3/movie/tt" + imdbId + \
 		"/credits?api_key=" + apiKey
-	credits = json.load(urllib2.urlopen(url))
+	request = urllib.request.Request(url)
+	response = urllib.request.urlopen(request)	
+	credits = json.load(response)
 	time.sleep(0.25)
 	director = ""
 	for cast in credits["cast"]:
@@ -57,7 +61,7 @@ def getCreditsInfo(imdbId):
 
 def getDecade(movieInfo):
 	decade = str(int(math.floor(int(movieInfo["release_date"][0:4]) / 10) * 10)) + 's'
-	print "- " + decade
+	print(" - " + decade, flush=True)
 	decadeList[decade] = decadeList.get(decade, 0) + 1
 
 def getCountry(movieInfo):
@@ -78,28 +82,27 @@ def generateMovieInfos():
 				continue
 			watchDate, imdbId, rating, useEnglishTitle = movie.strip().split(" ")
 			if movie:
-				print imdbId,
+				print(imdbId, end='', flush=True)
 				movieInfo = getMovieInfo(imdbId)
 				movieInfoLight = dict()			
 				if useEnglishTitle == "1":
 					movieInfoLight["original_title"] = movieInfo["title"]
 				else:
 					movieInfoLight["original_title"] = movieInfo["original_title"]	
-				print "-> " + movieInfoLight["original_title"].encode('utf-8'),
+				print("-> " + movieInfoLight["original_title"], end='', flush=True)
 				getDecade(movieInfo)
 				getCountry(movieInfo)
 				getGenre(movieInfo)
 				director = getCreditsInfo(imdbId)
 				movieInfoLight["imdb_id"] = movieInfo["imdb_id"]					
 				movieInfoLight["release_date"] = movieInfo["release_date"][0:4]
-				movieInfoLight["vote_average"] = movieInfo["vote_average"]
+				#movieInfoLight["vote_average"] = movieInfo["vote_average"]
 				movieInfoLight["watchDate"] = watchDate
 				movieInfoLight["personalRating"] = rating
 				movieInfoLight["runtime"] = movieInfo["runtime"]
 				movieInfoLight["poster_path"] = movieInfo["poster_path"]
 				movieInfoLight["director"] = director.strip()
 				movieInfos.append(movieInfoLight)
-				sys.stdout.flush()
 
 	jsonResult["countries"] = countryList
 
@@ -109,13 +112,13 @@ def generateMovieInfos():
 	genreListSorted = sorted(genreList.items(), key=operator.itemgetter(1), reverse=True)
 	jsonResult["genres"] = collections.OrderedDict(genreListSorted)
 
-	actorListSorted = sorted(actorList.items(), key=operator.itemgetter(1), reverse=True)
+	actorListSorted = sorted(actorList.items(), key=lambda x: x[1].get('count'), reverse=True)
 	jsonResult["actors"] = actorListSorted[0:10]
 
-	directorListSorted = sorted(directorList.items(), key=operator.itemgetter(1), reverse=True)
+	directorListSorted = sorted(directorList.items(), key=lambda x: x[1].get('count'), reverse=True)
 	jsonResult["directors"] = directorListSorted
 
-	cameraListSorted = sorted(cameraList.items(), key=operator.itemgetter(1), reverse=True)
+	cameraListSorted = sorted(cameraList.items(), key=lambda x: x[1].get('count'), reverse=True)
 	jsonResult["cinematographers"] = cameraListSorted[0:10]
 
 	jsonResult["movies"] = movieInfos
